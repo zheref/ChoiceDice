59aeff2fd844611ce9ec48f0c31a853f398df97a
Sergio Daniel <me@zheref.io>
2023-07-08 15:09:49 -0500

    Add texta area from addRoll

--- choice-dice-ionic/src/ui/pages/AddRoll/index.tsx
+++ choice-dice-ionic/src/ui/pages/AddRoll/index.tsx
@@ -1,13 +1,20 @@
-import { IonPage, IonText } from "@ionic/react"
+import { IonContent, IonHeader, IonItem, IonPage, IonText, IonTextarea, IonTitle, IonToolbar } from "@ionic/react"
 
-const AddRoll: React.FC = () => {
-    return (
-        <IonPage>
-            <IonText>
-                <h3>Create Page</h3>
-            </IonText>
-        </IonPage>
-    ) 
-}
+const AddRoll: React.FC = () => (
+    <IonPage>
+
+        <IonHeader>
+            <IonToolbar>
+                <IonTitle>Add new roll</IonTitle>
+            </IonToolbar>
+           
+        </IonHeader>
+        <IonContent>
+            <IonItem>
+                <IonTextarea label="Roll" labelPlacement="floating"></IonTextarea>
+            </IonItem>
+        </IonContent>  
+    </IonPage>
+)
 
 export default AddRoll;
